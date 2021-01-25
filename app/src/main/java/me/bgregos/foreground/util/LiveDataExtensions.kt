80b58f2a84d09b06ae1b31923d9b0cd78370f97f
Brendan Gregos <bgregos@gmail.com>
2021-01-24 21:39:33 -0500

    simplify a name

--- app/src/main/java/me/bgregos/foreground/util/LiveDataExtensions.kt
+++ app/src/main/java/me/bgregos/foreground/util/LiveDataExtensions.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.LiveData
  * A wrapper around MutableLiveData that enforces non-nullability of its data
  */
 @Suppress("UNCHECKED_CAST")
-class NonNullableMutableLiveData<T>(value: T) : LiveData<T>(value) {
+class NonNullableLiveData<T>(value: T) : LiveData<T>(value) {
     override fun getValue(): T = super.getValue() as T
     public override fun setValue(value: T) = super.setValue(value)
     public override fun postValue(value: T) = super.postValue(value)
