83a5b6ddb5038af4ccf081bff987e17fe7574316
Brendan Gregos <bgregos@gmail.com>
2021-03-24 23:31:30 -0400

    build filters layouts

--- app/src/main/java/me/bgregos/foreground/filter/FiltersFragment.kt
+++ app/src/main/java/me/bgregos/foreground/filter/FiltersFragment.kt
@@ -8,9 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import me.bgregos.foreground.R
+import me.bgregos.foreground.databinding.FragmentFiltersBinding
+import me.bgregos.foreground.databinding.FragmentTaskDetailBinding
 import javax.inject.Inject
 
 class FiltersFragment : Fragment() {
+
+    private var _binding: FragmentFiltersBinding? = null
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = FiltersFragment()
@@ -21,9 +27,14 @@ class FiltersFragment : Fragment() {
 
     private val viewModel by viewModels<FiltersViewModel> { viewModelFactory }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_filters, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        _binding = FragmentFiltersBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+
+        rootView
+
+        return rootView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
